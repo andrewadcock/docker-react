87a1873e441d68efae2fe1d7562b8ee85c719bbc
Andrew Adcock <andrewadcock@gmail.com>
2020-02-09 13:14:28 -0500

    Add ssh key

--- deploy.sh
+++ deploy.sh
@@ -13,13 +13,20 @@ echo $(pwd)
   #cd /var/repo/andrewadcock
   git init
 
-  git remote add deploy "root@165.227.179.245:/var/repo/andrewadcock.git"
+  git remote add deploy "travis@165.227.179.245:/var/repo/andrewadcock.git"
   git config user.name "Andrew Adcock"
   git config user.email "andrewadcock+travis@gmail.com"
 
   git add .
   git status # debug
   git commit -m "Deploy"
+
+
+  openssl aes-256-cbc -K $encrypted_efba7c7df9d2_key -iv $encrypted_efba7c7df9d2_iv -in travis_rsa.enc -out travis_rsa -d
+  eval "$(ssh-agent -s)"
+  chmod 600 travis_rsa
+  ssh-add travis_rsa
+
   git push --force deploy HEAD:master
 else
   echo "Not deploying, since this branch isn't master."
